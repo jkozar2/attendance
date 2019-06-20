58f95a5ec99f8f1bc315e5867f320dc67ce576cf
Joseph Kozar <jkozar2@udayton.edu>
2019-06-20 10:12:05 -0400

    SAKAI-4637 to hold incomplete changes, combining Grading/Settings, that will be reversed for now.

--- tool/src/java/org/sakaiproject/attendance/tool/pages/SettingsPage.java
+++ tool/src/java/org/sakaiproject/attendance/tool/pages/SettingsPage.java
@@ -16,17 +16,25 @@
 package org.sakaiproject.attendance.tool.pages;
 
 
+import org.apache.wicket.AttributeModifier;
 import org.apache.wicket.RestartResponseException;
 import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.ajax.form.AjaxFormComponentUpdatingBehavior;
+import org.apache.wicket.ajax.markup.html.form.AjaxCheckBox;
 import org.apache.wicket.ajax.markup.html.form.AjaxSubmitLink;
 import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.markup.html.basic.Label;
-import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.markup.html.form.*;
+import org.apache.wicket.model.CompoundPropertyModel;
+import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.Model;
 import org.apache.wicket.model.ResourceModel;
 import org.sakaiproject.attendance.model.AttendanceSite;
 import org.sakaiproject.attendance.tool.panels.AttendanceCommentFormPanel;
+import org.sakaiproject.attendance.tool.panels.AttendanceGradeFormPanel;
 import org.sakaiproject.attendance.tool.panels.AttendanceStatusFormPanel;
+import org.sakaiproject.attendance.tool.panels.GradingRulesPanel;
+import org.sakaiproject.attendance.tool.panels.util.GradebookItemNameValidator;
 
 /**
  * SettingsPage gets various settings which affect the whole AttendanceSite
@@ -37,6 +45,13 @@ import org.sakaiproject.attendance.tool.panels.AttendanceStatusFormPanel;
  */
 public class SettingsPage extends BasePage {
 	private static final long serialVersionUID = 1L;
+	private                 boolean         previousSendToGradebook;
+	private                 String          previousName;
+	private                 Double          previousMaxGrade;
+	private        final IModel<Boolean> useAutoGrading              = new Model<>();
+	private        final    IModel<Boolean> autoGradeBySubtraction      = new Model<>();
+	private GradingRulesPanel gradingRulesPanel;
+	private                 WebMarkupContainer autoGradingTypeContainer;
 	
 	public SettingsPage() {
 		disableLink(settingsLink);
@@ -47,38 +62,153 @@ public class SettingsPage extends BasePage {
 
 		Label headerSettings = new Label("header-settings",	new ResourceModel("attendance.settings.header"));
 		add(headerSettings);
+		//Model<AttendanceSite> siteModel = new Model<>(attendanceLogic.getCurrentAttendanceSite());
+		//Form<AttendanceSite> settingsForm = new Form<~>("settings-form", new CompoundPropertyModel<>(attendanceLogic.getCurrentAttendanceSite())
+		Form<AttendanceSite> settingsForm = new Form<AttendanceSite>("settings-form", new CompoundPropertyModel<>(attendanceLogic.getCurrentAttendanceSite())){
+			@Override
+			protected void onSubmit() {
+				AttendanceSite aS = (AttendanceSite) getDefaultModelObject();
+				aS.setAttendanceStatuses();
+				aS.setUseAutoGrading(useAutoGrading.getObject());
+				aS.setAutoGradeBySubtraction(autoGradeBySubtraction.getObject());
 
-		Model<AttendanceSite> siteModel = new Model<>(attendanceLogic.getCurrentAttendanceSite());
-		Form<AttendanceSite> settingsForm = new Form<>("settings-form", siteModel);
+				if(aS.getMaximumGrade() == null && previousMaxGrade != null) {
+					aS.setSendToGradebook(false);
+					aS.setUseAutoGrading(false);
+				}
 
+				boolean result = attendanceLogic.updateAttendanceSite(aS);
 
-		settingsForm.add(createEditStatusesPanel(siteModel));
-		settingsForm.add(createEditCommentPanel(siteModel));
+				if (result) {
+					if(aS.getSendToGradebook()){
+						if(previousSendToGradebook) { // if previously true, see if any relevant values have changed
+							if(!previousName.equals(aS.getGradebookItemName()) || !previousMaxGrade.equals(aS.getMaximumGrade())){
+								attendanceGradebookProvider.update(aS);
+							}
+
+							previousName = aS.getGradebookItemName();
+						} else {
+							attendanceGradebookProvider.create(aS);
+						}
+					} else {
+						if(previousSendToGradebook) {
+							attendanceGradebookProvider.remove(aS);
+						}
+					}
+
+					previousMaxGrade = aS.getMaximumGrade();
+					previousSendToGradebook = aS.getSendToGradebook();
+
+					// Successful Save - Regrade All if Auto Grade is set to true and maximum points is set
+					if (aS.getUseAutoGrading() != null && aS.getUseAutoGrading() && aS.getMaximumGrade() > 0) {
+						attendanceLogic.regradeAll(aS);
+					}
+
+					getSession().info(getString("attendance.settings.grading.success"));
+				} else {
+					getSession().error(getString("attendance.settings.grading.failure"));
+				}
+				//super.onSubmit(target, form);
+				//target.add(feedbackPanel);
+			}
+		};
+
+		settingsForm.add(createEditStatusesPanel());
+		settingsForm.add(createEditCommentPanel());
+		createEditGradePanel(settingsForm);
 
 		AjaxSubmitLink submit = new AjaxSubmitLink("submit-link") {
+			@Override
+			protected void onError(AjaxRequestTarget target, Form<?> form) {
+				target.add(feedbackPanel);
+			}
+
 			@Override
 			protected void onSubmit(AjaxRequestTarget target, Form<?> form) {
 				super.onSubmit(target, form);
 				target.add(feedbackPanel);
 			}
-
 		};
 		settingsForm.add(submit);
-
+		// Grade rules container
+		this.gradingRulesPanel = new GradingRulesPanel("grading-rules");
+		this.gradingRulesPanel.setOutputMarkupPlaceholderTag(true);
+		add(this.gradingRulesPanel);
 		add(settingsForm);
-
 	}
 
-	private WebMarkupContainer createEditStatusesPanel(Model<AttendanceSite> siteModel) {
-
+	private WebMarkupContainer createEditStatusesPanel() {
 		WebMarkupContainer allStatusesContainer = new WebMarkupContainer("all-statuses-container");
-		allStatusesContainer.add(new AttendanceStatusFormPanel("edit-status-panel", feedbackPanel, siteModel));
+		allStatusesContainer.add(new AttendanceStatusFormPanel("edit-status-panel", feedbackPanel));
 		return allStatusesContainer;
 	}
 
-	private WebMarkupContainer createEditCommentPanel(Model<AttendanceSite> siteModel) {
+	private WebMarkupContainer createEditCommentPanel() {
 		WebMarkupContainer commentContainer = new WebMarkupContainer("comment-container");
-		commentContainer.add(new AttendanceCommentFormPanel("edit-comment-panel", feedbackPanel, siteModel));
+		commentContainer.add(new AttendanceCommentFormPanel("edit-comment-panel", feedbackPanel));
 		return commentContainer;
+	}
+
+	private void createEditGradePanel(Form<AttendanceSite> settingsForm) {
+		final AttendanceSite aS = attendanceLogic.getCurrentAttendanceSite();
+		this.previousSendToGradebook = aS.getSendToGradebook();
+		this.previousName = aS.getGradebookItemName();
+		this.previousMaxGrade = aS.getMaximumGrade();
+		this.useAutoGrading.setObject(aS.getUseAutoGrading());
+		this.autoGradeBySubtraction.setObject(aS.getAutoGradeBySubtraction());
+
+		final WebMarkupContainer grading = new WebMarkupContainer("grading");
+		grading.setOutputMarkupPlaceholderTag(true);
+		Label maxGradeLabel = new Label("maximum-grade-label", new ResourceModel("attendance.settings.grading.max.points.possible"));
+		NumberTextField<Double> maximum = new NumberTextField<Double>("maximumGrade");
+		maximum.setMinimum(0.1);
+		maximum.setStep(0.1);
+		settingsForm.add(maximum);
+		settingsForm.add(grading);
+		settingsForm.add(maxGradeLabel);
+		Label isGradeShownLabel = new Label("is-grade-shown-label", new ResourceModel("attendance.settings.grading.is.grade.shown"));
+		CheckBox isGradeShown = new CheckBox("isGradeShown");
+		grading.add(isGradeShown);
+		grading.add(isGradeShownLabel);
+
+		final WebMarkupContainer gradebook = new WebMarkupContainer("gradebook");
+		gradebook.setOutputMarkupPlaceholderTag(true);
+		Label gbItemName = new Label("gradebook-item-name", new ResourceModel("attendance.settings.grading.gradebook.item.name"));
+		TextField<String> gradebookItemName = new TextField<String>("gradebookItemName");
+		gradebookItemName.add(new GradebookItemNameValidator(aS, aS.getGradebookItemName()));
+		gradebookItemName.setRequired(true);
+		gradebook.add(gbItemName);
+		gradebook.add(gradebookItemName);
+		grading.add(gradebook);
+		final boolean gradebookDefined = attendanceGradebookProvider.isGradebookDefined(aS.getSiteID());
+		final AjaxCheckBox sendToGradebook = new AjaxCheckBox("sendToGradebook", Model.of(this.previousSendToGradebook)) {
+			@Override
+			protected void onUpdate(AjaxRequestTarget target) {
+				int thing = 0;
+			}
+		};
+		Label sendToGBLabel = new Label("send-to-gradebook", new ResourceModel("attendance.settings.grading.send.to.gradebook"));
+		grading.add(sendToGradebook);
+		grading.add(sendToGBLabel);
+		final RadioGroup<Boolean> useAutoGradingGroup = new RadioGroup<>("use-auto-grading-group", this.useAutoGrading);
+		useAutoGradingGroup.setRenderBodyOnly(false);
+		grading.add(useAutoGradingGroup);
+		Radio<Boolean> manualGrading = new Radio<>("manual-grading", Model.of(Boolean.FALSE));
+		Radio<Boolean> autoGrading = new Radio<>("auto-grading", Model.of(Boolean.TRUE));
+		useAutoGradingGroup.add(manualGrading);
+		useAutoGradingGroup.add(autoGrading);
+		this.autoGradingTypeContainer = new WebMarkupContainer("auto-grading-type");
+		this.autoGradingTypeContainer.setOutputMarkupPlaceholderTag(true);
+		grading.add(this.autoGradingTypeContainer);
+		final RadioGroup<Boolean> autoGradeType = new RadioGroup<>("auto-grading-type-group", this.autoGradeBySubtraction);
+		autoGradeType.setRenderBodyOnly(false);
+		this.autoGradingTypeContainer.add(autoGradeType);
+		Radio<Boolean> subtractGrading = new Radio<>("subtract-grading", Model.of(Boolean.TRUE));
+		Radio<Boolean> addGrading = new Radio<>("add-grading", Model.of(Boolean.FALSE));
+		autoGradeType.add(subtractGrading);
+		autoGradeType.add(addGrading);
+		settingsForm.add(grading);
+
+		//return new AttendanceGradeFormPanel("grade-settings-panel", feedbackPanel);
 	}
 }
--- tool/src/java/org/sakaiproject/attendance/tool/panels/AttendanceRecordFormDataPanel.java
+++ tool/src/java/org/sakaiproject/attendance/tool/panels/AttendanceRecordFormDataPanel.java
@@ -20,6 +20,7 @@ import org.apache.wicket.Component;
 import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.apache.wicket.ajax.form.AjaxFormSubmitBehavior;
 import org.apache.wicket.ajax.markup.html.form.AjaxSubmitLink;
+import org.apache.wicket.behavior.AttributeAppender;
 import org.apache.wicket.markup.head.IHeaderResponse;
 import org.apache.wicket.markup.head.JavaScriptHeaderItem;
 import org.apache.wicket.markup.head.OnDomReadyHeaderItem;
@@ -128,7 +129,32 @@ public class AttendanceRecordFormDataPanel extends BasePanel {
                 });
                 ajaxTargets.add(statusRadio);
                 statusRadio.setLabel(Model.of(getStatusString(itemStatus)));
+                statusRadio.setLabel(Model.of("")); //second label for creating a colored box
+                SimpleFormComponentLabel clickBox = new SimpleFormComponentLabel("record-status-box", statusRadio);
+                AttributeAppender iconMaker = null;
+                switch (getStatusString(itemStatus)){   //make icon based on what status it is.
+                    case "Present":
+                        iconMaker = new AttributeAppender("class", " fa fa-check");
+                        break;
+                    case "Absent":
+                        iconMaker = new AttributeAppender("class", " fa fa-times");
+                        break;
+                    case "Excused":
+                        iconMaker = new AttributeAppender("class", " fa fa-genderless");
+                        break;
+                    case "Late":
+                        iconMaker = new AttributeAppender("class", " fa fa-bed");
+                        break;
+                    case "Left Early":
+                        iconMaker = new AttributeAppender("class", " fa fa-hand-peace-o");
+                        break;
+                    default:
+                        iconMaker = new AttributeAppender("class", " fa fa-check");
+                        break;
+                }
+                clickBox.add(iconMaker);
                 item.add(new SimpleFormComponentLabel("record-status-name", statusRadio));
+                item.add(clickBox);
                 item.add(new Label("record-status-name-raw", itemStatus.toString()));
             }
         };
